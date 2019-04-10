cfc737e1145c4dd1e6751aa50e5eb1c8106730af
rg-ci <ci@realglobe.jp>
2019-04-10 01:56:59 +0000

    [ci skip] Travis CI committed after build

---
+++ test/eslint_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for eslint.
+ * Runs with mocha.
+ */
+'use strict'
+
+const eslint = require('../lib/eslint.js')
+const assert = require('assert')
+
+describe('eslint', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Eslint', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
